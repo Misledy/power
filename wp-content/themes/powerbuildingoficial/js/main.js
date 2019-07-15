b2dc87e33b4c8271618d00ce8bc1bf72ea6c6c56
Misledy Alastre <mislejose@gmail.com>
2019-07-15 09:01:18 -0430

    Agregar nuevas imágenes de votaciones

--- wp-content/themes/powerbuildingoficial/js/main.js
+++ wp-content/themes/powerbuildingoficial/js/main.js
@@ -1,19 +1,19 @@
 (function(){
-  jQuery('.carousel-showmanymoveone .item').each(function(){
-      var itemToClone = jQuery(this);
+    $('.carousel-showmanymoveone .item').each(function(){
+      var itemToClone = $(this);
   
       for (var i=1;i<6;i++) {
         itemToClone = itemToClone.next();
   
         // wrap around if at end of item collection
         if (!itemToClone.length) {
-          itemToClone = jQuery(this).siblings(':first');
+          itemToClone = $(this).siblings(':first');
         }
   
         // grab item, clone, add marker class, add to collection
         itemToClone.children(':first-child').clone()
           .addClass("cloneditem-"+(i))
-          .appendTo(jQuery(this));
+          .appendTo($(this));
       }
     });
   }());
